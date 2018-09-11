efc8ffccae3a2a456ebc0fb2cd68ec88ce1ec104
Psychotheus <psychotheus@gmail.com>
2018-09-12 01:08:25 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  //K=[N /(5^1)] +  [N /(5^2)] + [N /(5^3)] +...........+  [N /(5^n)]
+  let zeroCount = 0;
+  let pow = 1;
+  let K = 0;
+
+  do {
+    K = Math.trunc(number / Math.pow(5, pow));
+    zeroCount += K;
+    pow++;
+  } while (K > 1)
+
+  return zeroCount;
 }
